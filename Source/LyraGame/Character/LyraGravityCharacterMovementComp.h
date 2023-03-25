a28333c064454fbe7bbc8bb4ce5c05aa9397781f
AdamUAT <adawhite@uat.edu>
2023-03-24 17:23:33 -0700

    Initial Commit

---
+++ Source/LyraGame/Character/LyraGravityCharacterMovementComp.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/LyraCharacterMovementComponent.h"
+#include "LyraGravityCharacterMovementComp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LYRAGAME_API ULyraGravityCharacterMovementComp : public ULyraCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
